a733661e818e324607f205c0d1655f2be6846bc8
Woojin Lim <dnwls4031@naver.com>
2022-07-12 18:20:56 +0900

    study changing color

---
+++ app.js
@@ -0,0 +1,54 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function startPainting(){
+    painting = true;
+}
+
+function stopPainting(){
+    painting = false;
+}
+
+function onMouseDown(event){
+    painting=true;
+}
+
+function onMouseUp(event){
+    stopPainting();
+}
+
+function handleColorClick(event){
+
+}
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    } else{
+        ctx.lineTo(x,y)
+        ctx.stroke();
+        
+    }
+}
+
+
+if(canvas){
+    canvas.addEventListener("mousemove",onMouseMove);
+    canvas.addEventListener("mousedown",startPainting);
+    canvas.addEventListener("mouseup",stopPainting);
+    canvas.addEventListener("mouseleave",stopPainting);
+}
+
+Array.from(colors).forEach(color => color.addEventListener("click"));
